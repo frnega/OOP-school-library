ba144a9945201dffaedc96e30c9349f215f9a0f8
Frehiywot Nega <fr.nega@gmail.com>
2023-02-01 15:22:58 +0300

    fix to lint error

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
-    def correct_name
-      raise NotImplementedError, "You must implement #{self.class}##{__method__}"
-    end
+  def correct_name
+    raise NotImplementedError, "You must implement #{self.class}##{__method__}"
   end
+end
