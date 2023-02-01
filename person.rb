815a635695a40d2e2255f66bb501e60c9af6b4ed
Frehiywot Nega <fr.nega@gmail.com>
2023-02-01 16:06:39 +0300

    set up associations

--- person.rb
+++ person.rb
@@ -2,13 +2,14 @@ require_relative 'nameable'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age , :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -17,6 +18,11 @@ class Person < Nameable
 
   def correct_name
     name
+  end
+
+  def add_rental(rental)
+    @rentals << rental unless @rentals.include?(rental)
+    rental.person = self
   end
 
   private
