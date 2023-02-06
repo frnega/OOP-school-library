aba817f999982703a7e84e04853bd76bf6fcd727
Frehiywot Nega <fr.nega@gmail.com>
2023-02-06 14:06:19 +0300

    change to app.rb

--- app.rb
+++ app.rb
@@ -1,44 +1,119 @@
-require_relative 'classes/person'
-require_relative 'classes/student'
-require_relative 'classes/teacher'
-require_relative 'classes/book'
-require_relative 'classes/rental'
-require_relative 'classes/student_module'
-require_relative 'classes/teacher_module'
-require_relative 'classes/person_module'
-require_relative 'classes/book_module'
-require_relative 'classes/rental_module'
+require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'book'
+require_relative 'rental'
+
 class App
   def initialize
-    @books = BookModule.new
-    @students = StudentModule.new
-    @teachers = TeacherModule.new
-    @people = PersonModule.new(@students, @teachers)
-    @rentals = RentalModule.new(@books.books, @students.students, @teachers.teachers)
+    @books = []
+    @students = []
+    @teachers = []
+    @rentals = []
   end
 
   def list_books
-    @books.send(:list_books)
+    @books.each { |book| puts book }
+    puts
+  end
+
+  def people
+    [*@students, *@teachers]
   end
 
   def list_people
-    @people.send(:list_people)
+    people.each { |person| puts person }
+    puts
+  end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp
+
+    print 'Name: '
+    name = gets.chomp
+
+    print 'Has parent permission? [Y/N]: '
+    case gets.chomp.upcase
+    when 'Y'
+      parent_permission = true
+    when 'N'
+      parent_permission = false
+    else
+      puts 'That is not a valid input. Person creation failed.'
+      return
+    end
+
+    @students << Student.new(age, nil, name, parent_permission)
+  end
+
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp
+
+    print 'Name: '
+    name = gets.chomp
+
+    print 'Specialization: '
+    specialization = gets.chomp
+
+    @teachers << Teacher.new(age, specialization, name)
   end
 
   def create_person
-    @people.send(:create_person)
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+
+    case gets.chomp
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      puts 'That is not a valid input. Person creation failed.'
+      return
+    end
+
+    print "Person created successfully\n\n"
   end
 
   def create_book
-    @books.send(:create_book)
+    print 'Title: '
+    title = gets.chomp
+
+    print 'Author: '
+    author = gets.chomp
+
+    @books << Book.new(title, author)
+
+    print "Book created successfully\n\n"
   end
 
   def create_rental
-    @rentals.send(:create_rental)
+    puts 'Select a book from the following list by number'
+    @books.each_with_index { |book, index| puts "#{index}) #{book}" }
+    book_index = gets.chomp.to_i
+    puts
+
+    puts 'Select a person from the following list by number (not id)'
+    people.each_with_index { |person, index| puts "#{index}) #{person}" }
+    person_index = gets.chomp.to_i
+    puts
+
+    print 'Date: '
+    date = gets.chomp
+
+    @rentals << Rental.new(date, @books[book_index], people[person_index])
+
+    print "Rental created successfully\n\n"
   end
 
   def list_rentals
-    @rentals.send(:list_rentals)
+    print 'ID of person: '
+    id = gets.chomp.to_i
+
+    puts 'Rentals:'
+    @rentals.each { |rental| puts rental if rental.person.id == id }
+    puts
   end
 
   def exit_app
